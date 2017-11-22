99827175b2472010b709135a2608f4f1659c2b53
jiaming.shang <jiaming.shang@rea-group.com>
2017-11-22 23:19:05 +0800

    add fibonacci demo

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ module.exports.addNumber=(event,context) =>{
 module.exports.subNumber=(event,context) =>{
   context.succeed(event.n1-event.n2)
 }
+
+module.exports.fibonacci=(event,context) =>{
+  context.succeed({n1:event.n2,n2:event.n1+event.n2,num:event.num-1})
+}
